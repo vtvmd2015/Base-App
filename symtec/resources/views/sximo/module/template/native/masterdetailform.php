1c39764ee3ad55e1169a1f8d57ce633982b979b0
vtvmd2015 <vtvmd2015@gmail.com>
2015-12-28 19:21:03 -0800

    base code update
    28122015

---
+++ symtec/resources/views/sximo/module/template/native/masterdetailform.php
@@ -0,0 +1,121 @@
+<?php
+$tpl['masterdetailmodel'] = str_replace("/","","\$this->modelview = new  \App\Models\/".$module."();");
+$tpl['masterdetaildelete'] = '\DB::table(\''.$info['table'].'\')->whereIn(\''.$info['key'].'\',$request->input(\'ids\'))->delete();';
+$tpl['masterdetailform'] = '			
+	<hr />
+	<div class="clr clear"></div>
+	
+	<h5> '.$info['title'].' </h5>
+	
+	<div class="table-responsive">
+    <table class="table table-striped ">
+        <thead>
+			<tr>
+				@foreach ($subgrid[\'tableGrid\'] as $t)
+					@if($t[\'view\'] ==\'1\' && $t[\'field\'] !=\''.$info['master_key'].'\')
+						<th>{{ $t[\'label\'] }}</th>
+					@endif
+				@endforeach
+				<th></th>	
+			  </tr>
+
+        </thead>
+
+        <tbody>
+        @if(count($subgrid[\'rowData\'])>=1)
+            @foreach ($subgrid[\'rowData\'] as $rows)
+            <tr class="clone clonedInput">
+									
+			 @foreach ($subgrid[\'tableGrid\'] as $field)
+				 @if($field[\'view\'] ==\'1\' && $field[\'field\'] !=\''.$info['master_key'].'\')
+				 <td>					 
+				 	{!! SiteHelpers::bulkForm($field[\'field\'] , $subgrid[\'tableForm\'] , $rows->$field[\'field\']) !!}							 
+				 </td>
+				 @endif					 
+			 
+			 @endforeach
+			 <td>
+			 	<a onclick=" $(this).parents(\'.clonedInput\').remove(); return false" href="#" class="remove btn btn-xs btn-danger">-</a>
+			 	<input type="hidden" name="counter[]">
+			 </td>
+			@endforeach
+			</tr> 
+
+		@else
+            <tr class="clone clonedInput">
+									
+			 @foreach ($subgrid[\'tableGrid\'] as $field)
+
+				 @if($field[\'view\'] ==\'1\' && $field[\'field\'] !=\''.$info['master_key'].'\')
+				 <td>					 
+				 	{!! SiteHelpers::bulkForm($field[\'field\'] , $subgrid[\'tableForm\'] ) !!}							 
+				 </td>
+				 @endif					 
+			 
+			 @endforeach
+			 <td>
+			 	<a onclick=" $(this).parents(\'.clonedInput\').remove(); return false" href="#" class="remove btn btn-xs btn-danger">-</a>
+			 	<input type="hidden" name="counter[]">
+			 </td>
+			
+			</tr> 
+
+		
+		@endif	
+
+
+        </tbody>	
+
+     </table>  
+     <input type="hidden" name="enable-masterdetail" value="true">
+     </div>
+	<br /><br />
+     
+     <a href="javascript:void(0);" class="addC btn btn-xs btn-info" rel=".clone"><i class="fa fa-plus"></i> New Item</a>
+     <hr />';
+
+$tpl['masterdetailview'] ='	<hr />
+	
+	<h5> '.$info['title'].' </h5>
+	
+	<div class="table-responsive">
+    <table class="table table-striped ">
+        <thead>
+			<tr>
+				<th class="number"> No </th>
+					@foreach ($subgrid[\'tableGrid\'] as $t)
+					@if($t[\'view\'] ==\'1\')
+						<th>{{ $t[\'label\'] }}</th>
+					@endif
+				@endforeach
+				
+			  </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($subgrid[\'rowData\'] as $row)
+            <tr>
+				<td width="30">  </td>		
+			 @foreach ($subgrid[\'tableGrid\'] as $field)
+				 @if($field[\'view\'] ==\'1\' )
+				 <td>					 
+				 	@if($field[\'attribute\'][\'image\'][\'active\'] ==\'1\')
+						{!! SiteHelpers::showUploadedFile($row->$field[\'field\'],$field[\'attribute\'][\'image\'][\'path\']) !!}
+					@else	
+						{{--*/ $conn = (isset($field[\'conn\']) ? $field[\'conn\'] : array() ) /*--}}
+						{!! SiteHelpers::gridDisplay($row->$field[\'field\'],$field[\'field\'],$conn) !!}	
+					@endif						 
+				 </td>
+				 @endif					 
+			 
+			 @endforeach
+			@endforeach
+			</tr> 
+
+
+        </tbody>	
+
+     </table>   
+     </div>';     
+
+$tpl['masterdetailjs'] = '$(\'.addC\').relCopy({});';     
